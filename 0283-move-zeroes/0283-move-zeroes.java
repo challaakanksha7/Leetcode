89d76cc279629a99ac43a8447178a7fd634359db
challaakanksha <challaakanksha7@gmail.com>
2025-01-29 15:26:29 +0530

    Time: 2 ms (84.8%), Space: 46.2 MB (31.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -6,11 +6,13 @@ class Solution {
         int zero = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[zero];
-                nums[zero] = temp;
+                 nums[zero] = nums[i];
                 zero++;
             }
+        }
+        while(zero<nums.length){
+            nums[zero] = 0;
+            zero++;
         }
     }
 }
